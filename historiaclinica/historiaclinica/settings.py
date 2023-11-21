d99142015d2e258eec276b88609b5c0a0b30c5f8
Mad@23 <124703743+madeleinnehenao@users.noreply.github.com>
2023-11-21 12:59:04 -0500

    Aprendiendo git con sebastian

--- historiaclinica/historiaclinica/settings.py
+++ historiaclinica/historiaclinica/settings.py
@@ -29,8 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+THIRD_PARTY_APPS = [
+    'rest_framework'
+]
+LOCAL_APPS = [
+    'seguimientos',
+    'users'
+]
+
+INSTALLED_APPS = DJANGO_APPS+THIRD_PARTY_APPS+LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
