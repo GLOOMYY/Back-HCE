1f3c818d8183e93f1eb77a16942b3fdb608d7d43
Sebastian Mesa <sebas.mesa.montoya@gmail.com>
2023-11-21 13:18:19 -0500

    feat(users models): example

--- historiaclinica/seguimientos/models.py
+++ historiaclinica/seguimientos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+"""
+class Ejemplo(models.Model o lo que necesites heredar):
+    campo1 = models.CharField(verbose_name='Nombre del campo' ,max_length=50)
+    campo2 = models.ForeingKey(Para Relacionar Modelos)
+    
+    class Meta:
+        verbose_name = 'Ejemplo'
+        verbose_name_plural = 'Ejemplos'
+        
+    def __str__(self):
+        return f'Lo que se vaya a mostrar en el admin'
+"""
