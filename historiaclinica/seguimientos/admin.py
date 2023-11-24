75daed842b1472bb317a33a177c12467ee8af54b
Sebastian Mesa <sebas.mesa.montoya@gmail.com>
2023-11-24 17:02:42 -0500

    fix(admin): Error en import

--- historiaclinica/seguimientos/admin.py
+++ historiaclinica/seguimientos/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
 from .models import (
-    Formularios,
+    Formulario,
     SignosVitales,
     DatosAntropometricos,
     Observaciones,
     NotasAclaratorias,
+    SeguimientoEnfermeria,
+    SeguimientoMedicinaGeneral,
+    SeguimientoEspecialidad,
 )
 
 # Register your models here.
 
-admin.site.register(Formularios)
+admin.site.register(SeguimientoEnfermeria)
+admin.site.register(SeguimientoMedicinaGeneral)
+admin.site.register(SeguimientoEspecialidad)
+admin.site.register(Formulario)
 admin.site.register(SignosVitales)
 admin.site.register(DatosAntropometricos)
 admin.site.register(Observaciones)
-# admin.site.register(NotasAclaratorias)
+admin.site.register(NotasAclaratorias)
